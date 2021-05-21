2778d69d15d8ce4650a6f8f19786de2e5e34b2d3
am-steph <80233054+am-steph@users.noreply.github.com>
2021-05-21 11:30:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import random, time, os, schedule
 os.environ['TZ'] = 'Asia/China'
 
 def job():
-    randomSleep = random.randint(10,300)
-    print("Sleeping for: %ds" % randomSleep)
-    time.sleep(randomSleep)
     exec(open('./run.py').read())
 
 schedule.every().day.at("06:00").do(job)
